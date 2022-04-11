173a52cf93c836a111fdd31825ebb6183d8af1b5
Andre Matheus <am_arao@hotmail.com>
2022-04-11 10:48:26 -0300

    primeiro commit

---
+++ src/main/java/one/digitalinnovation/ListaCircular.java
@@ -0,0 +1,16 @@
+package one.digitalinnovation;
+
+public class ListaCircular<T> {
+
+    private No<T> cabeca;
+    private No<T> calda;
+    private int tamanhoLista;
+
+    public Boolean isEmpt(){
+        return this.tamanhoLista == 0 ? true : false;
+    }
+
+    public int size(){
+        return this.tamanhoLista;
+    }
+}
