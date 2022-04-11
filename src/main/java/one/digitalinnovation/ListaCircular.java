dd05c167b6a9012a34ef646a6e155924ae96c833
Andre Matheus <am_arao@hotmail.com>
2022-04-11 12:20:56 -0300

    metodos remove e add

--- src/main/java/one/digitalinnovation/ListaCircular.java
+++ src/main/java/one/digitalinnovation/ListaCircular.java
@@ -3,8 +3,56 @@ package one.digitalinnovation;
 public class ListaCircular<T> {
 
     private No<T> cabeca;
-    private No<T> calda;
+    private No<T> cauda;
     private int tamanhoLista;
+
+    public void add(T conteudo){
+        No<T> novoNo = new No<>(conteudo);
+        if(this.tamanhoLista == 0){
+            this.cabeca = novoNo;
+            this.cauda = this.cabeca;
+            this.cabeca.setNoProximo(cauda);
+        }else {
+            novoNo.setNoProximo(cauda);
+            this.cabeca.setNoProximo(novoNo);
+            this.cauda = novoNo;
+        }this.tamanhoLista++;
+    }
+
+    public void remove(int index){
+        if(index >= this.tamanhoLista)
+            throw new IndexOutOfBoundsException("O indice é maior que o tamanho");
+        No<T> noAuxiliar = this.cauda;
+        if(index == 0){
+            this.cauda = this.cauda.getNoProximo();
+            this.cabeca.setNoProximo(this.cauda);
+        }else if(index == 1){
+            this.cauda.setNoProximo(this.cauda.getNoProximo().getNoProximo());
+        }else{
+            for (int i = 0; i < index-1; i++){
+                noAuxiliar = noAuxiliar.getNoProximo();
+            }
+            noAuxiliar.setNoProximo(noAuxiliar.getNoProximo().getNoProximo());
+        }this.tamanhoLista--;
+    }
+
+    public T get(int index){
+        return this.getNo(index).getConteudo();
+    }
+
+    private No<T> getNo(int index){
+        if(this.isEmpt())
+            throw new IndexOutOfBoundsException("A lista está vazia");
+        if (index == 0) {
+                return this.cauda;
+            }
+            No<T> noAuxiliar = this.cauda;
+            for (int i = 0; (i < index) && (noAuxiliar != null); i++){
+            noAuxiliar = noAuxiliar.getNoProximo();
+
+        }
+        return noAuxiliar;
+    }
 
     public Boolean isEmpt(){
         return this.tamanhoLista == 0 ? true : false;
